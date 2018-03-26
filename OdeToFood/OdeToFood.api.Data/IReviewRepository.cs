936f99d51cdefb76a71d6c8fd0d0b841c071f16c
TeppersBen <Ben.Teppers@student.pxl.be>
2018-03-26 10:25:59 +0200

    OdeToFood - Part 2

---
+++ OdeToFood/OdeToFood.api.Data/IReviewRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.api.Data.DomainClasses;
+
+namespace OdeToFood.api.Data
+{
+    interface IReviewRepository
+    {
+        Task<IEnumerable<Review>> GetAllAsync();
+        Task<Review> GetByIdAsync(int id);
+        Task<Review> AddAsync(Review review);
+        Task UpdateAsync(Review review);
+        Task DeleteAsync(int id);
+    }
+}
